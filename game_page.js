836d6ed3d6fe5c19c6ecb4565ef7bead89e29d1c
megan-coder <81788505+megan-coder@users.noreply.github.com>
2021-10-21 18:58:22 -0400

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,5 +1,5 @@
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
+player1_name = localStorage.setItem("player1_name");
+player2_name = localStorage.setItem("player2_name");
 
 player1_score = 0;
 player2_score = 0;
